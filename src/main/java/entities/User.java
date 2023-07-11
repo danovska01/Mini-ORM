8edf3340b7c2bae05293c1a0e2d1b4aaf8dc95b4
kristina <kristina.danovska@gmail.com>
2023-07-11 19:05:49 +0300

    add User

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,63 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class User {
+    private int id;
+    private String userName;
+
+    private String password;
+
+    private int age;
+    private LocalDate registrationDate;
+
+
+    public User(String userName, String password, int age, LocalDate registrationDate) {
+        this.userName = userName;
+        this.password = password;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+
+}
